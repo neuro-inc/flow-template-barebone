1260fba6444c7bcf71fd05d512a3f294f851ac47
Andrii Homiak <homiakandrii@gmail.com>
2022-03-09 18:40:48 +0200

    feat: Add cookiecutter hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,11 @@
+--2022-03-09 18:28:11--  https://raw.githubusercontent.com/neuro-inc/cookiecutter-neuro-project/master/hooks/pre_gen_project.py
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.108.133, 185.199.109.133, 185.199.110.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.108.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 1352 (1,3K) [text/plain]
+Saving to: ‘pre_gen_project.py’
+
+     0K .                                                     100% 33,4M=0s
+
+2022-03-09 18:28:12 (33,4 MB/s) - ‘pre_gen_project.py’ saved [1352/1352]
+
